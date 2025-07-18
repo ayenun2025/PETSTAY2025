c1463d373bd9879fa6407b2adaab108320ddbd53
ayenun2025 <ayenun.naher1@gmail.com>
2025-07-18 12:09:18 -0400

    hi

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,19 +1,19 @@
 window.PETSTAY_CONFIG = {
-  AWS_REGION: "{{AWS_REGION}}",
-  COGNITO_USER_POOL_ID: "{{COGNITO_USER_POOL_ID}}",
-  COGNITO_USER_POOL_CLIENT_ID: "{{COGNITO_USER_POOL_CLIENT_ID}}",
-  COGNITO_DOMAIN: "{{COGNITO_DOMAIN}}",
-  REDIRECT_SIGN_IN_URL: "{{REDIRECT_SIGN_IN_URL}}",
-  REDIRECT_SIGN_OUT_URL: "{{REDIRECT_SIGN_OUT_URL}}",
-  BOOKINGS_API_URL: "{{BOOKINGS_API_URL}}",
-  ROOMS_AVAILABILITY_API_URL: "{{ROOMS_AVAILABILITY_API_URL}}",
-  NEW_BOOKING_API_URL: "{{NEW_BOOKING_API_URL}}",
-  CHECKIN_API_URL: "{{CHECKIN_API_URL}}"
+  AWS_REGION: 'us-east-1',
+  COGNITO_USER_POOL_ID: 'us-east-1_jaXO80Xah',
+  COGNITO_USER_POOL_CLIENT_ID: '3dpsa80ai1mrogcluofao2ejsk',
+  COGNITO_DOMAIN: 'https://us-east-1jaxo80xah.auth.us-east-1.amazoncognito.com',
+  REDIRECT_SIGN_IN_URL: '{{REDIRECT_SIGN_IN_URL}}',
+  REDIRECT_SIGN_OUT_URL: '{{REDIRECT_SIGN_OUT_URL}}',
+  BOOKINGS_API_URL: '{{BOOKINGS_API_URL}}',
+  ROOMS_AVAILABILITY_API_URL: '{{ROOMS_AVAILABILITY_API_URL}}',
+  NEW_BOOKING_API_URL: '{{NEW_BOOKING_API_URL}}',
+  CHECKIN_API_URL: '{{CHECKIN_API_URL}}'
 };
 
 // Safety check: crash the page if placeholders were not replaced
-for (const key in window.PETSTAY_CONFIG) {
-  if (window.PETSTAY_CONFIG[key].includes("{{") || window.PETSTAY_CONFIG[key].includes("}}")) {
-    throw new Error(`Missing config value: ${key}. Did you forget to set environment variables?`);
-  }
-}
+//for (const key in window.PETSTAY_CONFIG) {
+  //if (window.PETSTAY_CONFIG[key].includes("{{") || window.PETSTAY_CONFIG[key].includes("}}")) {
+    //throw new Error(`Missing config value: ${key}. Did you forget to set environment variables?`);
+ // }
+//}
